7d1275e48d7534db31a5e304f375d1744b904cf3
Raj <rzp5412@psu.edu>
2021-04-15 00:34:47 -0400

    initial

---
+++ helper.h
@@ -0,0 +1,10 @@
+
+//buffer structure. You are allowed to additional members in it. 
+//    DO NOT CHANGE ANY EXISTING MEMBER
+//    DO NOT CHANGE ANY EXISTING MEMBER
+
+
+typedef struct {
+    bool isopen;
+    fifo_t* fifoQ;
+} state_t;
